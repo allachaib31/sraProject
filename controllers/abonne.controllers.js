f3ba6ceffe0769f700a85cbb16a23dfdae62e9b4
nihal <kihalnihal48@gmail.com>
2023-12-02 09:50:07 +0100

    add unsebscribe router

--- controllers/abonne.controllers.js
+++ controllers/abonne.controllers.js
@@ -19,5 +19,26 @@ exports.abonne = async(req,res)=>{
         return res.status(200).send('err')
     }
 }
+exports.desabonne = async (req, res) => {
+    try{
+        const user = await userModel.findById(req.body.idUser)
+        const channel = user.subscribes.indexOf(req.body.idChannel);
+        for(let i=0;i<user.subscribes.length;i++){
+            if(user.subscribes[i]!=req.body.idChannel){
+                return res.status(200).send({
+                    msg : "not subscribe"
+                })
+            }
+        }
+        user.subscribes.splice(channel, 1);
+        await user.save();
+
+        return res.status(200).send({
+            msg: 'unsubscribe'
+        });
+    }catch(err){
+        return res.status(200).send('err');
+    }
+}
 
 
--- routers/abonne.routers.js
+++ routers/abonne.routers.js
@@ -3,5 +3,6 @@ const middleware = require('../middleware/authentication.middleware');
 const router = express.Router();
 const abonne = require('../controllers/abonne.controllers')
 
-router.post('/',middleware.authenticationMiddleware,abonne.abonne)
+router.post('/',middleware.authenticationMiddleware,abonne.abonne);
+router.post('/desabonne',middleware.authenticationMiddleware,abonne.abonne);
 module.exports = router;
